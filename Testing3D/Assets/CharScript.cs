364c06fbe257ea3e8c76ee240782527ba653cb00
TERRY\Terry <u3519936@connect.hku.hk>
2018-02-02 18:57:42 +0800

    basic unity with character movement

---
+++ Testing3D/Assets/CharScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharScript : MonoBehaviour {
+
+	Animator animChar;
+	Rigidbody mainChar;
+
+	// Use this for initialization
+	void Start () {
+		mainChar = GetComponent<Rigidbody> ();
+		animChar = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			mainChar.AddForce(new Vector3(0,0,5), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.forward);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			mainChar.AddForce(new Vector3(0,0,5), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.forward);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			mainChar.AddForce(new Vector3(-5,0,0), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.left);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			mainChar.AddForce(new Vector3(5,0,0), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.right);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			mainChar.AddForce(new Vector3(0,0,-5), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.back);
+		}
+			
+	}
+
+	void FixedUpdate(){
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			mainChar.AddForce(new Vector3(0,0,5), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.forward);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			mainChar.AddForce(new Vector3(0,0,5), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.forward);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			mainChar.AddForce(new Vector3(-5,0,0), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.left);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			mainChar.AddForce(new Vector3(5,0,0), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.right);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			mainChar.AddForce(new Vector3(0,0,-5), ForceMode.VelocityChange);
+			mainChar.rotation = Quaternion.LookRotation (Vector3.back);
+		}
+	}
+}
---
+++ Testing3D/Assets/SimplePeople3/Animations/PlayerControl.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+
+var speed : float = 10;
+
+function Update () 
+{
+	if (Input.GetKey(moveRight)) 
+	{
+		GetComponent.<Rigidbody>().velocity.x = speed;
+	}
+	else if (Input.GetKey(moveLeft))
+	{
+		GetComponent.<Rigidbody>().velocity.x = - speed;
+	}
+	else 
+	{
+		GetComponent.<Rigidbody>().velocity.x = 0;
+	}
+	
+	GetComponent.<Rigidbody>().velocity.y = 0;
+}
